e45ce2ea9d95b4487dfbc1bc210dce65afc70794
Josh <jcpsimmons@gmail.com>
2019-10-04 15:47:57 -0700

    ugh

--- ESX000/ESX000.js
+++ ESX000/ESX000.js
@@ -1,18 +1,71 @@
+function flipFlop(state) {
+  console.log(state);
+  if (state == 1) {
+    console.log("if");
+    $("#animation1")
+      .removeClass("enter")
+      .addClass("exit")
+      .one(
+        "webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend",
+        function() {
+          $(this)
+            .removeClass("exit")
+            .hide();
+        }
+      );
+    $("#animation2")
+      .removeClass("exit")
+      .addClass("enter")
+      .show()
+      .one(
+        "webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend",
+        function() {
+          $(this).removeClass("enter");
+        }
+      );
+  } else {
+    console.log("else");
+    $("#animation2")
+      .removeClass("enter")
+      .addClass("exit")
+      .one(
+        "webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend",
+        function() {
+          $(this)
+            .removeClass("exit")
+            .hide();
+        }
+      );
+    $("#animation1")
+      .removeClass("exit")
+      .addClass("enter")
+      .show()
+      .one(
+        "webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend",
+        function() {
+          $(this).removeClass("enter");
+        }
+      );
+  }
+}
+
 $(document).ready(function() {
-  var whichAnimation = 1;
-  window.setInterval(function() {
-    if (whichAnimation == 1) {
-      playAnimation = "#animation1";
-      stopAnimation = "#animation2";
-      whichAnimation = 2;
+  $("#animation2")
+    .removeClass("enter")
+    .addClass("exit")
+    .one(
+      "webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend",
+      function() {
+        $(this).removeClass("exit");
+      }
+    );
+  var state = 1;
+  setInterval(function() {
+    if (state == 1) {
+      state = 2;
     } else {
-      playAnimation = "#animation2";
-      stopAnimation = "#animation1";
-      whichAnimation = 1;
+      state = 1;
     }
-    console.log(playAnimation);
-    console.log(stopAnimation);
-    $(playAnimation).css("-webkit-animation-play-state", "running");
-    $(stopAnimation).css("-webkit-animation-play-state", "paused");
-  }, 5000);
+    flipFlop(state);
+  }, 4000);
 });
