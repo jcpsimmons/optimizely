c77fb9bd78ea25581bd76f6a4bc3fe2eaf5a9aaa
Josh <jcpsimmons@gmail.com>
2019-10-09 14:18:22 -0700

    x

--- ESX125/ESX125.js
+++ ESX125/ESX125.js
@@ -1,27 +1,18 @@
-$(document).ready(function() {
-  var facets = $("div.faceted-search-component .navbar-collapse")
-    .children(".nav")
-    .clone();
-
+function clickFacet() {
   $("div.faceted-search-component .navbar-collapse")
     .children(".nav")
-    .remove();
+    .click(function() {
+      window["optimizely"] = window["optimizely"] || [];
+      window["optimizely"].push({
+        type: "event",
+        eventName: "clickedFacet"
+      });
+    });
+}
 
-  facets = facets.slice(0, 5);
-
-  $("div.faceted-search-component .navbar-collapse").append(facets);
+$(document).ready(function() {
+  clickFacet();
+  window.EventBus.$on("productResultsUpdated", function(data) {
+    clickFacet();
+  });
 });
-
-// // EVENT BUS stuff for later
-
-// $(document).ready(function() {
-//     window.EventBus.$on("productResultsUpdated", function(data) {
-//       if (data["facet"] == "features") {
-//         window["optimizely"] = window["optimizely"] || [];
-//         window["optimizely"].push({
-//           type: "event",
-//           eventName: "clickFeaturesFacet"
-//         });
-//       }
-//     });
-//   });
