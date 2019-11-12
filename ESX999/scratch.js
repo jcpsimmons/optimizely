2fd17b24da5e6bb1b20fafb89212144509bef8c1
Josh <jcpsimmons@gmail.com>
2019-11-11 16:43:28 -0800

    x

--- ESX999/scratch.js
+++ ESX999/scratch.js
@@ -78,7 +78,6 @@ class Store {
 
 x = new Store(utag_data);
 await x.generateRestfulProductsData();
-console.log(x.apiData);
 // x.generateWordProbabilities();
 
 // `https://brm-core-0.brsrvr.com/api/v1/core/?account_id=5221&auth_key=o5xlkgn7my5fmr5c&domain_key=livingspaces_com&request_id=fd8d6a02a5764b7995c600e766a38bda&url=%2fbr-checker&_br_uid_2=uid%253D4961390647524%253Av%253D11.8%253Ats%253D1463613117510%253Ahc%253D3145&ptype=other&request_type=search&q=%${xxxx}%22&start=0&rows=13&search_type=keyword&fl=title,pid,url,price,sale_price,reviews,reviews_count,thumb_image&fq=product_attribute%3A%${xxxx}`
