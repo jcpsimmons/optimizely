75c76507f955461b1282c4b8b308012ed0d2f30b
Josh Simmons <001simmoj@pcsmac001009.local>
2019-12-31 09:27:15 -0800

    add full width banner esx147

--- ESX147/ESX147.js
+++ ESX147/ESX147.js
@@ -3,14 +3,16 @@ function nested() {
   var style = document.createElement("style");
   style.type = "text/css";
   style.innerHTML =
-    ".stickyStyle { position:sticky;top:0;width:100%;z-index:99;background-color:rgba(255,255,255,.9);  padding-top:2rem; } .dropdownMenus {background-color:rgba(255,255,255,.9); box-shadow:0 5.3px 3px -4px grey;}";
+    "body {overflow-x:hidden} .stickyStyle { position:sticky;top:0;width:100%; padding-top:2rem;z-index:1041} .dropdownMenus { padding-top: 0; } #floating-banner-bg { background-color:rgba(255,255,255,.9); border-bottom: 1px solid grey; position: absolute; top: 0; width: 300%; height: 11rem; transform: translateX(-50%); box-shadow: 0 5.3px 3px -4px grey; } @media screen and (max-width: 990px) { #floating-banner-bg { height: 13rem; } }";
   document.getElementsByTagName("head")[0].appendChild(style);
 
   // Get page components
   $("#plpServerSide .faceted-search-component").addClass("toWrap");
   $("#plpServerSide .row:first-of-type").addClass("toWrap");
 
-  $(".toWrap").wrapAll('<div id="NavContainer"></div>');
+  $("#NavContainer").length
+    ? null
+    : $(".toWrap").wrapAll('<div id="NavContainer"></div>');
 
   // Get the offset position of the navbar
   var sticky = document.querySelector("#NavContainer").offsetTop;
@@ -19,12 +21,35 @@ function nested() {
   function reposNav() {
     if (window.pageYOffset >= sticky) {
       $("#NavContainer").addClass("stickyStyle");
+      if (document.querySelector("#floating-banner-bg") == null)
+        $("#NavContainer > .row.toWrap").before(
+          '<div id="floating-banner-bg"></div>'
+        );
       $("#NavContainer .faceted-search-component").addClass("dropdownMenus");
       $("#NavContainer ul.pull-right").css("display", "none");
+      $("#NavContainer h1")
+        .css("font-size", "2rem")
+        .css("margin-bottom", "0");
+      $("#NavContainer .faceted-search-component .label-list.m-t-15").hide();
+      $("#NavContainer .clear-all").hide();
+      $("#NavContainer .filter-group-title").hide();
+      $("#NavContainer .faceted-search-component .applied-filter-wrapper")
+        .css("margin-top", "1.2rem")
+        .css("margin-bottom", "0");
     } else {
       $("#NavContainer").removeClass("stickyStyle");
+      $("#floating-banner-bg").remove();
       $("#NavContainer .faceted-search-component").removeClass("dropdownMenus");
       $("#NavContainer ul.pull-right").css("display", "block");
+      $("#NavContainer h1")
+        .css("font-size", "3rem")
+        .css("margin-bottom", "1.4rem");
+      $("#NavContainer .faceted-search-component .label-list.m-t-15").show();
+      $("#NavContainer .clear-all").show();
+      $("#NavContainer .filter-group-title").show();
+      $("#NavContainer .faceted-search-component .applied-filter-wrapper")
+        .css("margin-top", "0")
+        .css("margin-bottom", "1.2rem");
     }
   }
 
@@ -40,3 +65,5 @@ function nested() {
 }
 
 nested();
+
+$(".page-content").before('<div id="NavbarContainer"></div>');
